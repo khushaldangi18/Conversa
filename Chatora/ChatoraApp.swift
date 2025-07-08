6d95f10369e24888601696695d907e7ba4694f8a
Kavishm24 <kavishmenaria09@gmail.com>
2025-07-08 09:53:45 +0530

    initial commit

---
+++ Chatora/ChatoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatoraApp.swift
+//  Chatora
+//
+//  Created by FCP 21 on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
